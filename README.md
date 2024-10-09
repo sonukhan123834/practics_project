f05b1403ef82a17ffebe0cf7e16764b6edd9dd47
sonukhan123834 <sonukha43@gmail.com>
2024-10-09 11:56:20 +0530

    somenewupdated

--- README.md
+++
@@ -1,4 +0,0 @@
-# practics_project
-this is my first project
-<br>
-author sonu khan saifi
--- work/test.py
+++
@@ -1,4 +0,0 @@
-print("hello")
-print("hello world")
-print("i am from delhi")
-print("i am doing practics on git")
